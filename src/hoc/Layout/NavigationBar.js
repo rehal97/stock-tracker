822268147ea4a5194938e6acafe4fa0e4fd46bfc
Amandeep Rehal <arehal4@gmail.com>
2020-11-24 17:40:23 -0800

    fixed positioning of navbar items

--- src/hoc/Layout/NavigationBar.js
+++ src/hoc/Layout/NavigationBar.js
@@ -1,28 +1,28 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+
+import "./NavigationBar.css";
 
 import AuthenticationButton from "../../components/Auth/AuthenticationButton";
 
 const NavigationBar = () => (
-  <Navbar
-    className="justify-content-center"
-    bg="dark"
-    variant="dark"
-    expand="lg"
-  >
+  <Navbar bg="dark" variant="dark" expand="lg">
     <Navbar.Brand href="/">Stock Tracker</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav>
+      <Nav className={"NavbarItems"}>
         <Nav.Link href="/markets">Markets</Nav.Link>
         <Nav.Link href="/crypto">Crypto</Nav.Link>
         <Nav.Link href="/watchlist">Watchlist</Nav.Link>
         <Nav.Link href="/search">Search</Nav.Link>
-        <AuthenticationButton />
       </Nav>
     </Navbar.Collapse>
+    <Form>
+      <AuthenticationButton />
+    </Form>
   </Navbar>
 );
 
