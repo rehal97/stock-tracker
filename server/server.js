f98164c1e45e6efd3a814894b194a17a58aa73bd
Amandeep Rehal <arehal4@gmail.com>
2021-01-22 21:56:46 -0800

    test for deployed server

--- server/server.js
+++ server/server.js
@@ -17,6 +17,10 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build"));
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
