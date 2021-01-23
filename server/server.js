ed56bc6118701a14f2eda75fa390bac26b3bcd95
Amandeep Rehal <arehal4@gmail.com>
2021-01-22 21:02:17 -0800

    added webpack for deployment

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build"));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
