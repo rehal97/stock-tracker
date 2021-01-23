e78620cdf17eb2b6ab71a81584f292425f79a20d
Amandeep Rehal <arehal4@gmail.com>
2021-01-22 22:59:03 -0800

    fixed server port

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
